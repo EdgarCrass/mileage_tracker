9101159765f9d86bff9af8e8f4adb6e9467fc2c7
Edgar Crass Jr <EJCRASS1@madisoncollege.edu>
2014-05-12 22:27:27 -0500

    Completed Final Exam requirements

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -1,36 +1,41 @@
 class MakesController < ApplicationController
-	def index
-		@makes = Make.all
-	end
-	def show
-	end
-	def new
-		@make = Make.new
-	end
-	def create
-		@make = Make.new(make_params)
-		if @make.save
-			flash[:success] = "Make created!"
-			redirect_to makes_path
-		else
-			render :new
-		end
-	end
-	def edit
-		@make = Make.find(params[:id])
-	end
-	def update
-		@make = Make.find(params[:id])
-		if @make.update_attributes(make_params)
-			flash[:success] = "Make edited!"
-			redirect_to makes_path
-		else
-			render :edit
-		end
-	end
-	
-	private
-		def make_params
-			params.require(:make).permit(:name)
-		end
+  def index
+    @makes = Make.all
+  end
+
+  def show
+    @make = Make.find params[:id]
+  end
+
+  def new
+    @make = Make.new
+  end
+
+  def create
+    @make = Make.new(make_params)
+    if @make.save
+      redirect_to makes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @make = Make.find params[:id]
+  end
+
+  def update
+    @make = Make.find params[:id]
+    if @make.update_attributes(make_params)
+      redirect_to make_path(@make)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def make_params
+    params.require(:make).permit(:name)
+  end
 end
